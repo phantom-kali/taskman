0893447d5ae491c7d056490af401c10f75633fed
okalmary254 <okalmohn18@gmail.com>
2025-07-25 10:51:31 +0300

    added a temporary local storage to avoid file corruption and option to export task as csv

--- taskman.c
+++ taskman.c
@@ -2,115 +2,163 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 #define MAX_TASK_LENGTH 256
 #define MAX_TASKS 100
 #define FILENAME "tasks.txt"
+#define TEMPFILE "tasks.tmp"
 
-typedef struct {
+typedef enum
+{
+    TODO = 0,
+    DONE = 1
+} Status;
+
+typedef struct
+{
     int id;
     char description[MAX_TASK_LENGTH];
-    int completed;
+    Status completed;
     time_t created;
 } Task;
 
-typedef struct {
+typedef struct
+{
     Task tasks[MAX_TASKS];
     int count;
 } TaskManager;
 
 TaskManager tm = {0};
 
-void load_tasks() {
+void load_tasks()
+{
     FILE *file = fopen(FILENAME, "r");
-    if (!file) return;
-    
-    while (tm.count < MAX_TASKS && 
-           fscanf(file, "%d|%d|%ld|%[^\n]\n", 
+    if (!file)
+    {
+        perror("Failed to open task file");
+        return;
+    }
+
+    while (tm.count < MAX_TASKS &&
+           fscanf(file, "%d|%d|%ld|%[^]\n",
                   &tm.tasks[tm.count].id,
-                  &tm.tasks[tm.count].completed,
+                  (int *)&tm.tasks[tm.count].completed,
                   &tm.tasks[tm.count].created,
-                  tm.tasks[tm.count].description) == 4) {
+                  tm.tasks[tm.count].description) == 4)
+    {
         tm.count++;
     }
     fclose(file);
 }
 
-void save_tasks() {
-    FILE *file = fopen(FILENAME, "w");
-    if (!file) {
+void save_tasks()
+{
+    FILE *temp = fopen(TEMPFILE, "w");
+    if (!temp)
+    {
         printf("Error: Cannot save tasks\n");
         return;
     }
-    
-    for (int i = 0; i < tm.count; i++) {
-        fprintf(file, "%d|%d|%ld|%s\n",
+
+    for (int i = 0; i < tm.count; i++)
+    {
+        fprintf(temp, "%d|%d|%ld|%s\n",
                 tm.tasks[i].id,
                 tm.tasks[i].completed,
                 tm.tasks[i].created,
                 tm.tasks[i].description);
     }
-    fclose(file);
+    fclose(temp);
+    remove(FILENAME);
+    rename(TEMPFILE, FILENAME);
 }
 
-int get_next_id() {
+int get_next_id()
+{
     int max_id = 0;
-    for (int i = 0; i < tm.count; i++) {
-        if (tm.tasks[i].id > max_id) {
+    for (int i = 0; i < tm.count; i++)
+    {
+        if (tm.tasks[i].id > max_id)
+        {
             max_id = tm.tasks[i].id;
         }
     }
     return max_id + 1;
 }
 
-void add_task(const char *description) {
-    if (tm.count >= MAX_TASKS) {
+void add_task(const char *description)
+{
+    if (tm.count >= MAX_TASKS)
+    {
         printf("Error: Maximum tasks reached\n");
         return;
     }
-    
+
+    if (strlen(description) == 0 || strspn(description, " \t\n") == strlen(description))
+    {
+        printf("Error: Task description cannot be empty.\n");
+        return;
+    }
+
     Task *task = &tm.tasks[tm.count];
     task->id = get_next_id();
     strncpy(task->description, description, MAX_TASK_LENGTH - 1);
     task->description[MAX_TASK_LENGTH - 1] = '\0';
-    task->completed = 0;
+    task->completed = TODO;
     task->created = time(NULL);
-    
+
     tm.count++;
     save_tasks();
     printf("Task added: #%d - %s\n", task->id, task->description);
 }
 
-void list_tasks(int show_completed) {
-    if (tm.count == 0) {
+int cmp_created(const void *a, const void *b)
+{
+    return ((Task *)a)->created - ((Task *)b)->created;
+}
+
+void list_tasks(int show_completed)
+{
+    if (tm.count == 0)
+    {
         printf("No tasks found.\n");
         return;
     }
-    
-    printf("\n%-4s %-6s %-20s %s\n", "ID", "Status", "Created", "Description");
+
+    qsort(tm.tasks, tm.count, sizeof(Task), cmp_created);
+    printf("\n%-4s %-8s %-20s %s\n", "ID", "Status", "Created", "Description");
     printf("------------------------------------------------------------\n");
-    
-    for (int i = 0; i < tm.count; i++) {
+    int displayed = 0;
+
+    for (int i = 0; i < tm.count; i++)
+    {
         Task *task = &tm.tasks[i];
-        
-        if (!show_completed && task->completed) continue;
-        
+        if (!show_completed && task->completed == DONE)
+            continue;
+
         char time_str[20];
         struct tm *tm_info = localtime(&task->created);
         strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M", tm_info);
-        
-        printf("%-4d %-6s %-20s %s\n",
+
+        printf("%-4d \033[0;%sm%-8s\033[0m %-20s %s\n",
                task->id,
-               task->completed ? "[DONE]" : "[TODO]",
+               task->completed == DONE ? "32" : "33",
+               task->completed == DONE ? "[DONE]" : "[TODO]",
                time_str,
                task->description);
+        displayed++;
     }
+    printf("\nTotal tasks displayed: %d\n", displayed);
 }
 
-void complete_task(int id) {
-    for (int i = 0; i < tm.count; i++) {
-        if (tm.tasks[i].id == id) {
-            tm.tasks[i].completed = 1;
+void complete_task(int id)
+{
+    for (int i = 0; i < tm.count; i++)
+    {
+        if (tm.tasks[i].id == id)
+        {
+            tm.tasks[i].completed = DONE;
             save_tasks();
             printf("Task #%d marked as completed!\n", id);
             return;
@@ -119,11 +167,22 @@ void complete_task(int id) {
     printf("Task #%d not found.\n", id);
 }
 
-void delete_task(int id) {
-    for (int i = 0; i < tm.count; i++) {
-        if (tm.tasks[i].id == id) {
-            // Shift remaining tasks
-            for (int j = i; j < tm.count - 1; j++) {
+void delete_task(int id)
+{
+    for (int i = 0; i < tm.count; i++)
+    {
+        if (tm.tasks[i].id == id)
+        {
+            printf("Are you sure you want to delete task #%d? (y/n): ", id);
+            char confirm = getchar();
+            getchar(); // consume newline
+            if (confirm != 'y' && confirm != 'Y')
+            {
+                printf("Cancelled.\n");
+                return;
+            }
+            for (int j = i; j < tm.count - 1; j++)
+            {
                 tm.tasks[j] = tm.tasks[j + 1];
             }
             tm.count--;
@@ -135,64 +194,100 @@ void delete_task(int id) {
     printf("Task #%d not found.\n", id);
 }
 
-void show_help() {
+void edit_task(int id, const char *new_description)
+{
+    for (int i = 0; i < tm.count; i++)
+    {
+        if (tm.tasks[i].id == id)
+        {
+            strncpy(tm.tasks[i].description, new_description, MAX_TASK_LENGTH - 1);
+            tm.tasks[i].description[MAX_TASK_LENGTH - 1] = '\0';
+            save_tasks();
+            printf("Task #%d updated.\n", id);
+            return;
+        }
+    }
+    printf("Task #%d not found.\n", id);
+}
+
+void show_help()
+{
     printf("\nSimple Task Manager\n");
     printf("==================\n");
     printf("Usage:\n");
     printf("  ./taskman add \"Task description\"    - Add a new task\n");
-    printf("  ./taskman list                      - List pending tasks\n");
-    printf("  ./taskman list-all                  - List all tasks\n");
-    printf("  ./taskman done <id>                 - Mark task as completed\n");
-    printf("  ./taskman delete <id>               - Delete a task\n");
-    printf("  ./taskman help                      - Show this help\n\n");
+    printf("  ./taskman list                         - List pending tasks\n");
+    printf("  ./taskman list-all                     - List all tasks\n");
+    printf("  ./taskman done <id>                    - Mark task as completed\n");
+    printf("  ./taskman delete <id>                  - Delete a task\n");
+    printf("  ./taskman edit <id> \"new description\" - Edit a task\n");
+    printf("  ./taskman help                         - Show this help\n\n");
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     load_tasks();
-    
-    if (argc < 2) {
+
+    if (argc < 2)
+    {
         show_help();
         return 1;
     }
-    
-    if (strcmp(argv[1], "add") == 0) {
-        if (argc < 3) {
+
+    if (strcmp(argv[1], "add") == 0)
+    {
+        if (argc < 3)
+        {
             printf("Error: Please provide task description\n");
-            printf("Usage: ./taskman add \"Task description\"\n");
             return 1;
         }
         add_task(argv[2]);
-        
-    } else if (strcmp(argv[1], "list") == 0) {
+    }
+    else if (strcmp(argv[1], "list") == 0)
+    {
         list_tasks(0);
-        
-    } else if (strcmp(argv[1], "list-all") == 0) {
+    }
+    else if (strcmp(argv[1], "list-all") == 0)
+    {
         list_tasks(1);
-        
-    } else if (strcmp(argv[1], "done") == 0) {
-        if (argc < 3) {
+    }
+    else if (strcmp(argv[1], "done") == 0)
+    {
+        if (argc < 3)
+        {
             printf("Error: Please provide task ID\n");
-            printf("Usage: ./taskman done <id>\n");
             return 1;
         }
         complete_task(atoi(argv[2]));
-        
-    } else if (strcmp(argv[1], "delete") == 0) {
-        if (argc < 3) {
+    }
+    else if (strcmp(argv[1], "delete") == 0)
+    {
+        if (argc < 3)
+        {
             printf("Error: Please provide task ID\n");
-            printf("Usage: ./taskman delete <id>\n");
             return 1;
         }
         delete_task(atoi(argv[2]));
-        
-    } else if (strcmp(argv[1], "help") == 0) {
+    }
+    else if (strcmp(argv[1], "edit") == 0)
+    {
+        if (argc < 4)
+        {
+            printf("Usage: ./taskman edit <id> \"new description\"\n");
+            return 1;
+        }
+        edit_task(atoi(argv[2]), argv[3]);
+    }
+    else if (strcmp(argv[1], "help") == 0)
+    {
         show_help();
-        
-    } else {
+    }
+    else
+    {
         printf("Unknown command: %s\n", argv[1]);
         show_help();
         return 1;
     }
-    
+
     return 0;
 }
